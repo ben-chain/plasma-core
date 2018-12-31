1bfe9d2f16006fc0aeca9d3e7b4dfd7bd6949445
ben-chain <ben@pseudonym.party>
2018-12-31 14:03:12 -0500

    add new history checking logic

--- src/services/proof-service.js
+++ src/services/proof-service.js
@@ -1,9 +1,192 @@
 const BaseService = require('./base-service')
+const BN = require('web3').utils.BN
+const _ = require('lodash')
 
 class ProofSerivce extends BaseService {
   get name () {
     return 'proof-service'
   }
+
+  //TODO: replace start and ends with typedstarts and typedends
+
+  // deposits form: [{range, block, depositer}]
+  // history form: history[TRIndex(of Transaction being checked)][deposit][block][i] = {ith relevant tx, ITS transferIndex, [its tree indexes], [its [branches]]}
+  // ^ AKA history[TRIndex] = subHistory with subHistory[deposit][block][i] = as above
+  checkProof (transaction, history) {
+    if (!checkHistoryWellFormed(transaction, history)) return false
+    //TODO: make sure no trickery with sender or recipient = 0x0000000...
+    for (let i = 0; i < transaction.TransferRecords.elements.length; i++) { // for each send in the TX
+      const transfer = transaction.transfers[i]
+      let trHistory = history[i] // history for this particular TR
+      const sender = transfer.sender
+      const range = {start: transfer.start, end: transfer.end}
+      const deposits = getMostRecentDeposits(range)
+      for (let i = 0; i < deposits.length; i++) {
+        const deposit = deposits[i]
+        const depositHistory = trHistory[i]
+        depositHistory.toBlock = transaction.block // the block the history goes "up to" -- the transaction's block!
+        owner = checkDepositSubrangeOwner(deposit, subRange, depositHistory)
+        if (owner !== sender) return false
+      }
+    }
+    return true
+  }
+
+  //TODO: check all tx proofs upfront, including confirmAdjacentTransactions() for each blockHistory
+  //TODO check root sum is ffffff...
+  //TODO: check transaction inclusion in the block or decide to handle that before inputting here
+  //TODO break into more functions lololol
+  checkHistoryWellFormed (transaction, history) {
+    if (!history || !transaction) return false // something's missing
+    if (transaction.transfers.length !== history.length) return false // as many entries as TRs being covered from history
+    for (let i = 0; i < transaction.transfers.length; i++) { // for each transfer in the TX we're ultimately verifying...
+      const trHistory = history[i]
+      const transfer = transaction.transfers[i]
+      const deposits = getMostRecentDeposits(transfer.start, transfer.end)
+      if (deposits.length !== trHistory.length) return false 
+      for (let j = 0; j < deposits.length; j++) { // for each deposit covering that transfer...
+        const deposit = deposits[i]
+        const depositHistory = trHistory[j]
+        for (let block = deposit.block; k < transaction.block; k++) {
+          const blockProofs = depositHistory[block]
+          prevleafIndex = blockProofs[0].leafIndices[blockProofs[0].TRIndex] - 1
+          for (let k = 0; k < blockProofs.length; k++) {
+            const proof = blockProofs[k]
+            if 
+
+          }
+          // confirm depositHist[k] exitsts
+          // confirm tx's are adjacent
+          // confirm tx's are valid & well-formed including multisends
+        }
+
+      }
+    }
+    for (let subHistory in history) { // this is grabbing us the subhistory for each TR
+
+    }
+  }
+
+  checkDepositSubrangeOwner (deposit, subRange, depositHistory) {
+    let intersection = {}
+    intersection.start = (deposit.start.lt(subRange.start)) ? subRange.start : deposit.start // these two lines find the intersection between the deposit and the inquired range
+    intersection.end = (deposit.end.gt(subRange.end)) ? subRange.end : deposit.end
+    let rangeState = {range: intersection, owner: deposit.depositer} // initialize rangeState to all owned by depositer
+    for (let block = deposit.block; i < depositHistory.toBlock; i++) {
+      const blockHistory = depositHistory[block] // this is the most internal history element--represents the relevant proofs, for the transactions affecting the deposit range, at this block
+      const implicitRange = getImplicitRange(blockHistory) // full coverage of the blockHistory including implicit noTX's
+      if (implicitRange.start.gt(intersection.start) || implicitRange.end.lt(intersection.end)) return false // because then the proof doesn't cover ranges even with implicit noTXs!!! --> in(complete/valid) proof
+      for (let transactionProofs in blockHistory) {
+        const transfer = transactionProofs.transaction.transfers[transactionProofs.transferIndex]
+        rangeState = applyTransferToRangeState(transfer, rangeState)
+      }
+    }
+    if (rangeState.length === 1) return rangeState[0].owner 
+    else return false
+  }
+
+  // rangeState form: [{start, end, owner}]  <-- adjacent ones only!!! (enforced by client self-benevolence)
+  applyTransferToRangeState (transfer, rangeState) {
+    //todo add bound cutoffs
+    let overwritePoint = _.sortedIndexBy(rangeState, transfer, (range) => range.start.toString(16, 32)) // base 16, length 32  ==> 18 bytes
+    if (rangeState[overwritePoint+1].start.eq(transfer.start)) overwritePoint++ // if the two starts were equal, the sort tried to put ours first.  but we really want the thing being overwritten so ++
+    const oldRange = rangeState[overwritePoint]
+    if (oldRange.owner !== transfer.sender) return false // you didn't own it to send in the first place so gtfo
+    if (transfer.end.gt(oldRange.end)) return false // then transfer overlaps an ownership bound and is an invalid history
+    rangeState[overwritePoint].owner = transfer.recipient // these three lines replace the oldRange with our new one
+    rangeState[overwritePoint].start = transfer.start
+    rangeState[overwritePoint].end = transfer.end
+    if (transfer.end.eq(oldRange.end)) { // both had same end
+      if (transfer.owner === rangeState[overwritePoint + 1].owner) { // then we merge the ranges
+        rangeState[overwritePoint].end = rangeState[overwritePoint+1].end
+        rangeState.splice(overwritePoint + 1, 1) // remove the range we merged
+      }
+    } else { // end =/= end --> we gotta add back in the remaining range to the right!
+      rangeState.splice(overwritePoint + 1, 0, {start: transfer.end, end: oldRange.end, owner: oldRange.owner}) // add new element after our update for the remaining right side
+    }
+    if (transfer.start.eq(oldRange.start)) { // both had same start
+      if (transfer.owner === rangeState[overwritePoint - 1].owner) { // then we merge the ranges
+        rangeState[overwritePoint].start = rangeState[overwritePoint-1].start
+        rangeState.splice(overwritePoint, 1) // remove the range we merged
+      }
+    } else { // start =/= start --> we gotta add back in the remaining range to the left!
+      rangeState.splice(overwritePoint, 0, {start: oldRange.start, end: transfer.start, owner: oldRange.owner})
+    }
+    return rangeState
+  }
+
+  getImplicitRange (transactionProofs) {
+    let leftSum = rightSum = new BN(0)
+    const firstProofs = transactionProofs[0]
+    const firstBranch = firstProofs.branches[0]
+    const firstIndex = new BN(firstProofs.leafIndex).toString(2, firstBranch.length / 2)
+    const firstPath = firstIndex.split("").reverse().join("") // reverse the ordering so we start with the bottom
+    for (let i = 0; i < firstPath.length; i++) {
+      const bit = firstPath[i]
+      if (bit === '0') rightSum = rightSum.add(firstBranch[i+1].sum)
+    }
+    const lastProofs = transactionProofs[transactionProofs.length - 1]
+    const lastBranch = lastProofs.branches[lastProofs.branches.length - 1]
+    const lastIndex = new BN(lastProofs.leafIndex).toString(2, lastBranch.length / 2)
+    const lastPath = lastIndex.split("").reverse().join("") // reverse the ordering so we start with the bottom
+    for (let i = 0; i < firstPath.length; i++) {
+      const bit = lastPath[i]
+      if (bit === '1') leftSum = leftSum.add(lastBranch[i].sum)
+    }
+    return {start: leftSum, end: new BN('ffffffffffffffffffffffffffffffff',16).sub(rightSum)}
+  }
+
+  forBranch(leafIndex, proof, invokeThis) {
+    const index = new BN(leafIndex).toString(2, proof.length / 2) // path bitstring
+    const path = index.split("").reverse().join("") // reverse the ordering so we start with the bottom
+    for (let i = 0; i < path.length; i++) {
+      const bit = path[i]
+    }
+  }
+
+  const checkBranchValidity = function(leafIndex, transaction, proof, root) {
+    //todo check indexbitstring.length <= proof length, proof not empty, proof divides 2
+    const index = new BN(leafIndex).toString(2, proof.length / 2) // path bitstring
+    const path = index.split("").reverse().join("") // reverse ordering so we start with the bottom
+    let encoding = transaction.encode()
+    encoding = '0x' + new BN(encoding).toString(16, 2 * encoding.length)
+    const leafParent = (path[0] == '0') ? proof[0] : proof[1]
+    if ('0x' + leafParent.data.slice(0, 2 * 32) !== ST.hash(encoding)) return false // wasn't the right TX!
+    for (let i = 1; i < path.length; i++) {
+        const bit = path[i]
+        const potentialParent = (bit === '0') ? proof[2 * i] : proof[2 * i + 1]
+        const actualParent = ST.parent(proof[2 * (i - 1)], proof[2 * (i - 1) + 1])
+        if (!areNodesEquivalent(actualParent, potentialParent)) return false
+    }
+    const potentialRoot = (proof.length > 1) ? ST.parent(proof[proof.length-2], proof[proof.length-1]) : proof[proof.length]
+	//TODO check if sum is ffffffff
+    if (areNodesEquivalent(potentialRoot, root)) return true
+    return false // something didn't work!
+}
+
+
+
+  // TODO: actually use this in the initial checking of well-formed proofs
+  confirmAjacentTransactions (blockSubHistory) {
+    let ind = 0
+    for (transactionProof in blockSubHistory) {
+      if (transactionProof.index > ind) { // note: we will never have an equality here since we've verified the merkle sums above; so they prevent it! :)
+        ind = transactionProof.index
+      } else return false
+      return true
+    }
+  }
+  
+  //TODO: hardcode deposits for testing
+  //TODO later: replace hardcoding with ETH service & logic around it
+  getMostRecentDeposits(start, end) {
+    return [{range, depositer, block}]
+  }
+
+
+
+
+
 
   /**
    * Checks whether a transaction is valid or not.
